0b4295be2260fdce14f4582be6db90de9077d449
pawank06 <pawan06kumar2003@gmail.com>
2025-11-19 01:44:59 +0530

    first commit

---
+++ counter/src/lib.rs
@@ -0,0 +1,40 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{account_info::{AccountInfo, next_account_info}, entrypoint, entrypoint::{self, ProgramResult}, msg, pubkey::{self, Pubkey}};
+
+#[derive(BorshSerialize, BorshDeserialize)]
+enum InstructionType {
+    Increment(u32),
+    Decrement(u32)
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct Counter {
+    count: u32
+}
+
+entrypoint!(counter_program);
+fn counter_program(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instructions_data: &[u8]
+) -> ProgramResult{
+    let acc = next_account_info(&mut accounts.iter())?;
+    let instructions = InstructionType::try_from_slice(instructions_data)?;
+    let mut counter_data = Counter::try_from_slice(&acc.data.borrow())?;
+
+    match instructions {
+        InstructionType::Increment(value) => {
+            counter_data.count += value;
+            msg!("Count Increased");
+
+        }
+        InstructionType::Decrement(value) => {
+            counter_data.count -= value;
+            msg!("Count Decreased");
+        }
+    }
+
+    counter_data.count.serialize(&mut *acc.data.borrow_mut());
+    msg!("Contract Succeded");
+    Ok(())
+}
